36da9316c2b6280979200c895fea7d8d473a671c
Lance Guyatt <lance@lanceguyatt.com>
2012-09-27 11:09:27 +0100

    Swapped LESS for SASS

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+require '/Users/lance/.rvm/gems/ruby-1.8.7-p330/gems/susy-1.0.1/lib/susy.rb'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+output_style = :expanded
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
